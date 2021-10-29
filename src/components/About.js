dbd03539d2cace0fa4563f0bc58fdbb581776b21
Brianna Groden <briannaggroden@gmail.com>
2021-10-28 21:56:19 -0400

    styling 956

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,7 @@ const About = () => {
         <h1>about</h1>
         <p>The Electric Power Project (EPP) is a repository of information for the electrical industry with the mission of breaking down complex concepts to a digestable form. </p>
         <p>During early deployment of EPP the content will tailored for the electrical sales, but information remains beneficial across roles within the industry. </p>
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br />
       </div>
 
       <div className="disclaimer">
